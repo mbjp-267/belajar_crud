8be629ecfc29ba912c4d579fbb341dcd4b9c9db3
M. Bambang J. P. <bmbmanav09@gmail.com>
2025-07-23 21:03:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ try {
     console.error("Database connection error:", error);
 }
 
-app.use(cors());
+app.use(cors({
+    origin: "https://belajar-crud-lovat.vercel.app"
+}));
 app.use(express.json());
 app.use(UserRoute);
 
