0a03d6ee40c60f40033c406d8315a6914182289b
M. Bambang J. P. <bmbmanav09@gmail.com>
2025-07-25 17:14:54 +0700

    Update EditUser.js

--- src/components/EditUser.js
+++ src/components/EditUser.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import api from 'axios';
+import api from '../axios';
 import { useNavigate, useParams } from 'react-router-dom';
 
 const EditUser = () => {
