228f3ea1671f9b0a77faa48138ae1a513d560088
M. Bambang J. P. <bmbmanav09@gmail.com>
2025-07-25 10:19:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: "https://belajar-crud-lovat.vercel.app"
+  origin: "https://belajar-crud-lovat.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE"]
 }));
 app.use(express.json());
 app.use('/users', UserRoute);
