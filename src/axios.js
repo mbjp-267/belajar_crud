256d5fd4074c92cc1f80d9a621b38bf3170a3f47
M. Bambang J. P. <bmbmanav09@gmail.com>
2025-07-25 17:27:14 +0700

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "../axios";
 
 const instance = axios.create({
-  baseURL: "https://belajarcrud-production.up.railway.app"
+  baseURL: process.env.REACT_APP_API_URL
 });
 
 export default instance;
